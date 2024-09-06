116b8b3cf4332a68a4e90b4c871d318f33fc8ecc
Shashank Yadav <125982193+Shashank127Yadav@users.noreply.github.com>
2024-09-06 22:43:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const account1 = {
-  owner: "Jonas Schmedtmann",
+  owner: "MS Dhoni",
   movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
-  pin: 1111,
+  pin: 7,
 
   movementsDates: [
     "2019-11-18T21:31:17.178Z",
@@ -19,10 +19,10 @@ const account1 = {
 };
 
 const account2 = {
-  owner: "Jessica Davis",
+  owner: "Virat Kohli",
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
-  pin: 2222,
+  pin: 18,
 
   movementsDates: [
     "2019-11-01T13:15:33.035Z",
